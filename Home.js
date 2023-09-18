26f215ed04096e90d36342b5effb0ebd76939650
Hamit Mızrak <hamitmizrak@gmail.com>
2023-09-18 20:46:18 +0300

    revize

--- Home.js
+++
@@ -1,9 +0,0 @@
-import { Button, Text, View } from "react-native"
-
-
-export default ()=>(
-    <View>
-        <Text>Deneme</Text>
-        <Button title="Home Data" onPress={()=>25} />
-    </View>
-);
